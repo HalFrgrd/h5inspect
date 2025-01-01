6facbe45c743cc37fb290208cef35347aa4cfe4f
Hal Frigaard <4559349+HalFrgrd@users.noreply.github.com>
2025-01-01 15:29:34 +0000

    asd

--- src/h5_utils.rs
+++ src/h5_utils.rs
@@ -1,9 +1,10 @@
 use hdf5::{File, Result};
+use std::path::PathBuf;
 
-pub fn read_hdf5() -> Result<()> {
-    let file = File::open("dummy_data.h5")?; // open for reading
-    let ds = file.dataset("random_data")?; // open the dataset
-    let asd = ds.attr_names()?;
-    dbg!(asd);
-    Ok(())
-}
+// pub fn read_hdf5(file_path: &PathBuf) -> Result<()> {
+//     let file = File::open(file_path)?; // open for reading
+//     let ds = file.dataset("random_data")?; // open the dataset
+//     let asd = ds.attr_names()?;
+//     dbg!(asd);
+//     Ok(())
+// }
